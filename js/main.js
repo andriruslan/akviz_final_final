df3dcb1a9624c162bb459fd05114840a79ebd7a0
andriruslan <andriruslan1990@gmail.com>
2025-08-03 10:26:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+// js/main.js
+const loadComponent = async (id, path) => {
+  try {
+    const res = await fetch(path);
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
+  } catch (e) {
+    console.error("Помилка завантаження:", path);
+  }
+};
 
+window.onload = () => {
+  const components = [
+    "sector", "position", "location", "target-type", "side", "target-number",
+    "name", "count", "azimuth", "course", "distance", "height", "time",
+    "detection", "result", "ammo", "description", "preview", "actions", "compass-debug"
+  ];
+
+  components.forEach(name => {
+    loadComponent(`${name}-container`, `components/${name}.html`);
+  });
+};
